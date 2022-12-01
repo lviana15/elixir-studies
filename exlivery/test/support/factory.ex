d7aa19f461b052a2d461fb0a4376b8cd14583b3f
Lucas Viana <lucasviana0604@gmail.com>
2022-11-30 22:39:48 -0300

    Add: Item test

--- exlivery/test/support/factory.ex
+++ exlivery/test/support/factory.ex
@@ -1,6 +1,7 @@
 defmodule Exlivery.Factory do
   use ExMachina
 
+  alias Exlivery.Orders.Item
   alias Exlivery.Users.User
 
   def user_factory do
@@ -10,6 +11,15 @@ defmodule Exlivery.Factory do
       cpf: "12345678900",
       age: 20,
       address: "Rua Cidade, 167"
+    }
+  end
+
+  def item_factory do
+    %Item{
+      description: "Pizza de peperoni",
+      category: :pizza,
+      unity_price: Decimal.new("35.5"),
+      quantity: 1
     }
   end
 end
