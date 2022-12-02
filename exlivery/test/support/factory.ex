f85aedd1f8db4a2ce497fa82f85bfa6850b944e1
Lucas Viana <lucasviana0604@gmail.com>
2022-12-01 21:01:56 -0300

    Add Order test

--- exlivery/test/support/factory.ex
+++ exlivery/test/support/factory.ex
@@ -1,7 +1,7 @@
 defmodule Exlivery.Factory do
   use ExMachina
 
-  alias Exlivery.Orders.Item
+  alias Exlivery.Orders.{Item, Order}
   alias Exlivery.Users.User
 
   def user_factory do
@@ -20,6 +20,23 @@ defmodule Exlivery.Factory do
       category: :pizza,
       unity_price: Decimal.new("35.5"),
       quantity: 1
+    }
+  end
+
+  def order_factory do
+    %Order{
+      user_cpf: "12345678900",
+      delivery_address: "Rua Cidade, 167",
+      items: [
+        build(:item),
+        build(:item,
+         description: "Temaki de atum",
+         category: :japonesa,
+         quantity: 2,
+         unity_price: Decimal.new("20.50")
+        )
+      ],
+      total_price: Decimal.new("76.50")
     }
   end
 end
